1e7a5e19ecb0948d7991121b9a783566c5d261b0
M Yusuf Kurniawan <119839973+cupska@users.noreply.github.com>
2023-10-06 13:55:43 +0000

    #

---
+++ expense-tracker/app/records/page.tsx
@@ -0,0 +1,13 @@
+import { AiFillApple } from "react-icons/ai"
+function Records() {
+  return (
+    <>
+      <div className=" text-red ">awd</div>
+      <span className=" text-white ">
+        <AiFillApple />
+      </span>
+    </>
+  )
+}
+
+export default Records
